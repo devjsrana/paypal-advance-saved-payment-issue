b43bb342a7bb7640e5f1003fdb319b15c323c09c
devjsrana <ashrafujjamanrana@gmail.com>
2023-01-05 16:47:26 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Paypal advance payment and save card during purchase issue
+
+After first payment it saved my card successfully and return customer id, vault id. then created client token using customer id, it looks like this.
+![APP UI](/public/ui.png)
+
+After then when i try to pay with my saved card is show error
+![Error](/public/error.png)
---
+++ paypal-api.js
@@ -0,0 +1,99 @@
+import fetch from "node-fetch";
+
+// set some important variables
+const { CLIENT_ID, APP_SECRET } = process.env;
+const base = "https://api-m.sandbox.paypal.com";
+
+// call the create order method
+export async function createOrder(body) {
+  const purchaseAmount = "100.00"; // TODO: pull prices from a database
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders`;
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: body ? JSON.stringify({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: purchaseAmount,
+          },
+        },
+      ],
+      payment_source: {
+        card: {
+          name: body.card_name,
+          billing_address: body.billing_address,
+          attributes: {
+            vault: {
+              store_in_vault: 'ON_SUCCESS'
+            }
+          }
+        }
+      }
+    }) : undefined,
+  });
+
+  return handleResponse(response);
+}
+
+// capture payment for an order
+export async function capturePayment(orderId) {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return handleResponse(response);
+}
+
+// generate access token
+export async function generateAccessToken() {
+  const auth = Buffer.from(CLIENT_ID + ":" + APP_SECRET).toString("base64");
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: "post",
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${auth}`,
+    },
+  });
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+// generate client token
+export async function generateClientToken() {
+  const accessToken = await generateAccessToken();
+  const response = await fetch(`${base}/v1/identity/generate-token`, {
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Accept-Language": "en_US",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      customer_id: 'kxIaZbNwOZ'
+    })
+  });
+  const jsonData = await handleResponse(response);
+  return jsonData.client_token;
+}
+
+async function handleResponse(response) {
+  if (response.status === 200 || response.status === 201) {
+    return response.json();
+  }
+
+  const errorMessage = await response.text();
+  throw new Error(errorMessage);
+}
